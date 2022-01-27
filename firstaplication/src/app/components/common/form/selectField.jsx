75cbd4f80591b5337551e0efbf6427633bed46ad
Artur Kukharski <ya.artyrik@gmail.com>
2022-01-27 18:34:00 -0500

    add checkboxField, multiSelect, radioField

--- firstaplication/src/app/components/common/form/selectField.jsx
+++ firstaplication/src/app/components/common/form/selectField.jsx
@@ -4,15 +4,19 @@ import PropTypes from 'prop-types'
 const SelectField = ({
   label,
   value,
-  name,
   onChange,
   defaultOption,
   options,
-  error
+  error,
+  name
 }) => {
+  const handleChange = ({ target }) => {
+    onChange({ name: target.name, value: target.value })
+  }
   const getInputClasses = () => {
     return 'form-select' + (error ? ' is-invalid' : '')
   }
+
   const optionsArray =
     !Array.isArray(options) && typeof options === 'object'
       ? Object.keys(options).map(optionName => ({
@@ -20,6 +24,7 @@ const SelectField = ({
           value: options[optionName]._id
         }))
       : options
+
   return (
     <div className='mb-4'>
       <label htmlFor={name} className='form-label'>
@@ -30,33 +35,30 @@ const SelectField = ({
         id={name}
         name={name}
         value={value}
-        onChange={onChange}>
-        <option selected={options === ''} disabled value=''>
+        onChange={handleChange}>
+        <option disabled value=''>
           {defaultOption}
         </option>
         {optionsArray &&
           optionsArray.map(option => (
-            <option key={option.value} value={option.value}>
+            <option value={option.value} key={option.value}>
               {option.name}
             </option>
           ))}
       </select>
-      <div className='invalid-feedback'>{error}</div>
+      {error && <div className='invalid-feedback'>{error}</div>}
     </div>
   )
 }
 
-SelectField.defaultProps = {
-  defaultOption: 'Choose...'
-}
 SelectField.propTypes = {
   defaultOption: PropTypes.string,
   label: PropTypes.string,
-  name: PropTypes.string,
   value: PropTypes.string,
-  options: PropTypes.oneOfType([PropTypes.object, PropTypes.array]),
   onChange: PropTypes.func,
-  error: PropTypes.string
+  error: PropTypes.string,
+  options: PropTypes.oneOfType([PropTypes.object, PropTypes.array]),
+  name: PropTypes.string
 }
 
 export default SelectField
--- firstaplication/src/app/components/ui/registerForm.jsx
+++ firstaplication/src/app/components/ui/registerForm.jsx
@@ -1,19 +1,29 @@
 import React, { useEffect, useState } from 'react'
-import TextField from '../common/form/textField'
 import { validator } from '../../utils/validator'
+import TextField from '../common/form/textField'
 import api from '../../api'
 import SelectField from '../common/form/selectField'
+import RadioField from '../common/form/radioField'
+import MultiSelectField from '../common/form/multiSelectField'
+import CheckBoxField from '../common/form/checkBoxField'
 
 const RegisterForm = () => {
-  const [data, setData] = useState({ email: '', password: '', profession: '' })
-  const [errors, setErrors] = useState({})
+  const [data, setData] = useState({
+    email: '',
+    password: '',
+    profession: '',
+    sex: 'male',
+    qualities: [],
+    licence: false
+  })
+  const [qualities, setQualities] = useState({})
   const [professions, setProfession] = useState()
-
+  const [errors, setErrors] = useState({})
   useEffect(() => {
     api.professions.fetchAll().then(data => setProfession(data))
+    api.qualities.fetchAll().then(data => setQualities(data))
   }, [])
-
-  const handleChange = ({ target }) => {
+  const handleChange = target => {
     setData(prevState => ({
       ...prevState,
       [target.name]: target.value
@@ -41,22 +51,30 @@ const RegisterForm = () => {
       min: {
         message: 'Пароль должен состоять минимум из 8 символов',
         value: 8
-      },
-      profession: {
-        isRequired: {
-          message: 'Обязательно выберите вашу профессию'
-        }
+      }
+    },
+    profession: {
+      isRequired: {
+        message: 'Обязательно выберите вашу профессию'
+      }
+    },
+    licence: {
+      isRequired: {
+        message:
+          'Вы не можете использовать наш сервис без подтверждения лицензионного соглашения'
       }
     }
   }
-  useEffect(() => {
-    validate()
-  }, [data])
+
   const validate = () => {
     const errors = validator(data, validatorConfig)
     setErrors(errors)
     return Object.keys(errors).length === 0
   }
+
+  useEffect(() => {
+    validate()
+  }, [data])
   const isValid = Object.keys(errors).length === 0
 
   const handleSubmit = e => {
@@ -84,12 +102,38 @@ const RegisterForm = () => {
       />
       <SelectField
         label='Выбери свою профессию'
+        defaultOption='Choose...'
         options={professions}
         name='profession'
         onChange={handleChange}
         value={data.profession}
         error={errors.profession}
       />
+      <RadioField
+        options={[
+          { name: 'Male', value: 'male' },
+          { name: 'Female', value: 'female' },
+          { name: 'Other', value: 'other' }
+        ]}
+        value={data.sex}
+        name='sex'
+        onChange={handleChange}
+        label='Выберите ваш пол'
+      />
+      <MultiSelectField
+        options={qualities}
+        onChange={handleChange}
+        defaultValue={data.qualities}
+        name='qualities'
+        label='Выберите ваши качества'
+      />
+      <CheckBoxField
+        value={data.licence}
+        onChange={handleChange}
+        name='licence'
+        error={errors.licence}>
+        Подтвердить <a>лицензионное соглашение</a>
+      </CheckBoxField>
       <button
         className='btn btn-primary w-100 mx-auto'
         type='submit'
