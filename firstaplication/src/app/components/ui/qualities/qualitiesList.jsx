2b7a58e6f85be1883af3c2eb0c6446bf6ac23bee
Artur Kukharski <ya.artyrik@gmail.com>
2022-02-20 12:16:09 -0500

    add providers

--- firstaplication/src/app/components/ui/qualities/qualitiesList.jsx
+++ firstaplication/src/app/components/ui/qualities/qualitiesList.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 import PropTypes from "prop-types";
 import Quality from "./quality";
+import { useQualities } from "../../../hooks/useQuality";
 
-const QualitiesList = ({ qualities }) => {
-    return (
-        <>
-            {qualities.map((qual) => (
-                <Quality key={qual._id} {...qual} />
-            ))}
-        </>
-    );
+const QualitiesList = (qualitiesList) => {
+    const { isLoading, getQualityUser } = useQualities();
+    const qualities = getQualityUser(qualitiesList);
+    // console.log(qualities);
+    if (!isLoading) {
+        return qualities.map((qual) => <Quality key={qual._id} {...qual} />);
+    } else return "Loading...";
 };
 
 QualitiesList.propTypes = {
